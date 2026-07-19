4f903de5fdc202c8b18ddf676f4894c96a8c28ab
SPARK OFFICIAL <s20512165@gmail.com>
2026-07-19 16:53:31 +0530

    hii

--- api/index.php
+++ api/index.php
@@ -1,4 +1,3 @@
 <?php
-echo "<img src='https://images.unsplash.com/photo-1614200179396-2bdb77ebf81b?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bHV4dXJ5JTIwY2FyfGVufDB8fDB8fHww'>";
-echo "hii";
+echo "<img src='/car/car.png'>";
 ?>
